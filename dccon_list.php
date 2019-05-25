8a6bc7909ff7552a8f36e9adc608f32bd441b249
bongzreal <51012603+bongzreal@users.noreply.github.com>
2019-05-25 18:17:48 +0900

    Update dccon_list.php

--- dccon_list.php
+++ dccon_list.php
@@ -19,15 +19,14 @@
 "레바한참부족" : "https://bongzreal.github.io/레바콘/레바한참부족.png",
 "레바핥짝" : "https://bongzreal.github.io/레바콘/레바핥짝.png",
 
-"우리핵드럼" : "https://bongzreal.github.io//우리핵드럼.png",
-"우리핵박수" : "https://bongzreal.github.io//우리핵박수.gif",
-"우리핵박수2" : "https://bongzreal.github.io//우리핵박수2.gif",
-"우리핵박수3" : "https://bongzreal.github.io//우리핵박수3.gif",
-"우리핵빵야" : "https://bongzreal.github.io//우리핵빵야.gif",
-"우리핵쌍안경" : "https://bongzreal.github.io//우리핵쌍안경.gif",
-"우리핵아닌데" : "https://bongzreal.github.io//우리핵아닌데.gif",
-"우리핵안녕" : "https://bongzreal.github.io//우리핵안녕.gif",
-"우리핵투정" : "https://bongzreal.github.io//우리핵투정.gif",
-
+"우리핵드럼" : "https://bongzreal.github.io/우리핵콘/우리핵드럼.png",
+"우리핵박수" : "https://bongzreal.github.io/우리핵콘/우리핵박수.gif",
+"우리핵박수2" : "https://bongzreal.github.io/우리핵콘/우리핵박수2.gif",
+"우리핵박수3" : "https://bongzreal.github.io/우리핵콘/우리핵박수3.gif",
+"우리핵빵야" : "https://bongzreal.github.io/우리핵콘/우리핵빵야.gif",
+"우리핵쌍안경" : "https://bongzreal.github.io/우리핵콘/우리핵쌍안경.gif",
+"우리핵아닌데" : "https://bongzreal.github.io/우리핵콘/우리핵아닌데.gif",
+"우리핵안녕" : "https://bongzreal.github.io/우리핵콘/우리핵안녕.gif",
+"우리핵투정" : "https://bongzreal.github.io/우리핵콘/우리핵투정.gif",
 
 }
